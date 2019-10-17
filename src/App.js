69c78874bf17bcd774e66dc8afb9a2468930d8d0
Aaron-Fuller <aaron.fuller@protonmail.com>
2019-10-16 22:22:58 -0400

    Hero image added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ButtonAppBar from './components/Navi'
-
+//import SimpleCard from './components/SimpleCard'
+import Hero from './components/hero.jpg'
 
 function App() {
   return (
     <div className="App">
       <nav>
         <ButtonAppBar />
+        <img src={Hero} width="100%" height="600" alt="hero"/>
+        
       </nav>      
     </div>
   );
